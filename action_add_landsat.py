1e9615b62abed2d48469131fd6179c688ec95750
lmotta <motta.luiz@gmail.com>
2016-02-21 12:54:11 -0300

    Unified the types of providers

--- action_add_landsat.py
+++ action_add_landsat.py
@@ -1,13 +1,9 @@
-# Values from each features -- NOT CHANGE!
-#
-layer_id = '[% @layer_id %]'
-feat_id = [% "geocodmu" %]
-geomWkt = '[%geomToWKT(  $geometry  )%]'
-#
 # CHANGE HERE for your target
 #
+feat_filter = '[% "geocodmu" %]' # Value from action "geocodmu" is STRING!
+#
 nameModulus = "Action Add Landsat"
-layerSQL = "landsat_muni_%d" % feat_id
+layerSQL = "landsat_muni_%s" % feat_filter
 fileStyle = '/home/lmotta/data/qgis_qml/muni_landsat.qml'
 geomName = 'geom'
 select = """(
@@ -23,16 +19,16 @@ FROM
 INNER JOIN 
   "municipio_ibge"
 ON
-  "municipio_ibge"."geocodmu" = %d AND
+  "municipio_ibge"."geocodmu" = '%s' AND
   MbrIntersects("img_catalogo_landsat"."geom", "municipio_ibge"."geom") AND
   ST_Intersects("img_catalogo_landsat"."geom", "municipio_ibge"."geom")
-)""" % feat_id
+)""" % feat_filter
 #
 #
 # NOT CHANGE BELOW
 #
 from PyQt4.QtCore import QTimer, QFileInfo
-from PyQt4.QtGui import QColor
+from PyQt4.QtGui import QColor, QApplication
 
 import qgis
 from qgis.core import QgsDataSourceURI, QgsMapLayerRegistry, QgsSimpleLineSymbolLayerV2, QgsGeometry, QgsCoordinateTransform
@@ -89,32 +85,38 @@ class AddLayerSQL():
         layer.loadNamedStyle( self.fileStyleLayerSQL )
 
     msgBar = qgis.utils.iface.messageBar()
-    msgBar.pushMessage( self.nameModulus, "Adding %s" % self.nameLayerSQL, QgsMessageBar.INFO, 2 )
-    addStyle()
-    self.mlr.addMapLayer( layer )
+    if not layer.isValid():
+      clip = QApplication.clipboard()
+      clip.setText( self.select )
+      msg = "Layer query '%s' not valid. Invalid Query copied to Clipboard" % self.nameLayerSQL
+      msgBar.pushMessage( self.nameModulus, msg, QgsMessageBar.WARNING, 5 )
+    elif layer.featureCount() == 0L:
+      msg = "Not found register in Query '%s'!" % self.nameLayerSQL
+      msgBar.pushMessage( self.nameModulus, msg, QgsMessageBar.WARNING, 5 )
+    else:
+      msgBar.pushMessage( self.nameModulus, "Added %s" % self.nameLayerSQL, QgsMessageBar.INFO, 3 )
+      addStyle()
+      self.mlr.addMapLayer( layer )
+
     qgis.utils.iface.setActiveLayer( self.layerSrc )
     self._removeHighlight( 2 )
 
-  def addQueryByPG(self):
-    dataSourceUri = self.layerSrc.dataProvider().dataSourceUri()
-    uriSrc = QgsDataSourceURI( dataSourceUri )
+  def addLayer(self):
+    prov = self.layerSrc.dataProvider()
+    name =  prov.name()
+    uriSrc = QgsDataSourceURI( prov.dataSourceUri() )
     uri = QgsDataSourceURI()
-    uri.setConnection( uriSrc.host(), uriSrc.port(), uriSrc.database(), uriSrc.username(), uriSrc.password() )
-    uri.setDataSource( uriSrc.schema(), self.select, self.geomName )
-    #
-    self._addLayer( QgsVectorLayer( uri.uri(), self.nameLayerSQL, 'postgres') )
+    if uriSrc.host() == '':
+      uri.setDatabase( uriSrc.database() )
+    else:
+      uri.setConnection( uriSrc.host(), uriSrc.port(), uriSrc.database(), uriSrc.username(), uriSrc.password() )
 
-  def addQueryBySqlite(self):
-    dataSourceUri = self.layerSrc.dataProvider().dataSourceUri()
-    schema = ''
-    uri = QgsDataSourceURI()
-    uri.setDatabase( dataSourceUri.split('|')[0] )
-    uri.setDataSource( schema, self.select, self.geomName )
-    #
-    self._addLayer( QgsVectorLayer( uri.uri(), self.nameLayerSQL, 'spatialite' ) )
+    uri.setDataSource( uriSrc.schema(), self.select, self.geomName )
+    self._addLayer( QgsVectorLayer( uri.uri(), self.nameLayerSQL, name ) )
+
 #
-sqlProperties = { 'layer_id': layer_id, 'geomWkt': geomWkt, 'select': select, 'geomName': geomName }
+sqlProperties = { 'layer_id': '[% @layer_id %]', 'geomWkt': '[%geomToWKT(  $geometry  )%]', 'select': select, 'geomName': geomName }
 layerSqlProperties = { 'name': layerSQL, 'fileStyle': fileStyle }
 #
 alq = AddLayerSQL( nameModulus, sqlProperties, layerSqlProperties )
-alq.addQueryBySqlite()
+alq.addLayer()
